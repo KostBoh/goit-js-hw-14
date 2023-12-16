1b2f549f3d169a9ceb8f936356830c59911d5e84
KostBoh <bohdan.kostya@gmail.com>
2023-12-16 19:56:54 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = this.elements.email.value;
+  const password = this.elements.password.value;
+
+  if (email.trim() === "" || password.trim() === "") {
+    alert("Будь ласка, заповніть всі поля форми.");
+  } else {
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    console.log(formData);
+    this.reset();
+  }
+});
