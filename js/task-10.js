0a8b7e559121f58e9a3ae9576efb3294832e755f
KostBoh <bohdan.kostya@gmail.com>
2023-12-17 17:34:41 +0200

    add task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  const boxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = boxSize + i * 10;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+
+createButton.addEventListener("click", () => {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
