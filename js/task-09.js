bf1b304e85071be67a8ab239e751881729c1b2df
KostBoh <bohdan.kostya@gmail.com>
2023-12-17 11:59:24 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const spanColor = document.querySelector(".color");
+  const buttonChangeColor = document.querySelector(".change-color");
+  buttonChangeColor.addEventListener("click", function () {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    spanColor.textContent = randomColor;
+  });
+});
